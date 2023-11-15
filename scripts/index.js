4b4eef681799b5e477f45331de53b9e5192998a7
LucasHasting <110124874+LucasHasting@users.noreply.github.com>
2023-11-14 21:50:24 -0600

    Add files via upload

--- scripts/index.js
+++ scripts/index.js
@@ -1,5 +1,5 @@
 $(document).ready(function () {
-$(".box").on("click", function () {
+  $(".box").on("click", function () {
     console.log($(this));
     //change border color to green
     $(this).toggleClass('clicked');
@@ -14,13 +14,13 @@ $(".box").on("click", function () {
     });
 
     //if all items are selected, fade
-    if(box_count === 2){
+    if (box_count === 2) {
       $(".box").fadeOut(1500)
     }
-});
+  });
 });
 
-function reset(){
+function reset() {
   $(".box").each(function () {
     //if something is selected, unselect it
     if ($(this).css("border-color") === "rgb(0, 128, 0)") {
